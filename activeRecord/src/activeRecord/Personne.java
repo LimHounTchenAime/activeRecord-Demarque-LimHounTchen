5be03cb17938d1303b03189ac18826ee29a6d3cc
2Mo <71017381+Amaury258@users.noreply.github.com>
2022-11-21 18:01:41 +0100

    Finalisation de la classe Personne

--- activeRecord/src/activeRecord/Personne.java
+++ activeRecord/src/activeRecord/Personne.java
@@ -1,5 +1,7 @@
 package activeRecord;
 
+import com.mysql.cj.x.protobuf.MysqlxPrepare;
+
 import java.sql.*;
 import java.util.ArrayList;
 
@@ -24,8 +26,12 @@ public class Personne {
         Statement stmt = connect.createStatement();
         ResultSet rs = stmt.executeQuery(queryString);
         ArrayList<Personne> tab = new ArrayList<>();
+        Personne p = null;
         while(rs.next()){
-            tab.add(new Personne(rs.getString("NOM"),rs.getString("PRENOM")));
+            p = new Personne(rs.getString("NOM"),rs.getString("PRENOM"));
+            p.id = rs.getInt("ID");
+            tab.add(p);
+
         }
         return tab;
     }
@@ -40,6 +46,7 @@ public class Personne {
         ResultSet rs = stmt.executeQuery(queryString);
         if(rs.next()){
             p = new Personne(rs.getString("NOM"),rs.getString("PRENOM"));
+            p.id = rs.getInt("ID");
         }
         return p;
     }
@@ -51,10 +58,72 @@ public class Personne {
         PreparedStatement stmt = connect.prepareStatement(queryString);
         ResultSet rs = stmt.executeQuery(queryString);
         ArrayList<Personne> tab = new ArrayList<>();
+        Personne p = null;
         while(rs.next()){
-            tab.add(new Personne(rs.getString("NOM"),rs.getString("PRENOM")));
+            p = new Personne(rs.getString("NOM"),rs.getString("PRENOM"));
+            p.id = rs.getInt("ID");
+            tab.add(p);
         }
         return tab;
+    }
+
+    public static void createTable() throws SQLException{
+        Connection connect=DBConnection.getConnection();
+        String createString = "CREATE TABLE personne ( " + "ID INTEGER  AUTO_INCREMENT, "
+                + "NOM varchar(40) NOT NULL, " + "PRENOM varchar(40) NOT NULL, " + "PRIMARY KEY (ID))";
+        Statement stmt = connect.createStatement();
+        stmt.executeUpdate(createString);
+    }
+
+    public static void deleteTable() throws SQLException{
+        Connection connect=DBConnection.getConnection();
+        String createString = "DROP TABLE IF EXISTS personne";
+        Statement stmt = connect.createStatement();
+        stmt.executeUpdate(createString);
+    }
+
+    public void delete() throws SQLException {
+        Connection connect=DBConnection.getConnection();
+        String query = "DELETE FROM personne WHERE id = ?";
+        PreparedStatement stmt = connect.prepareStatement(query);
+        stmt.setInt(1,this.id);
+        stmt.executeUpdate();
+        this.id=-1;
+    }
+
+    public void save() throws SQLException {
+        if(this.id==-1){
+            this.saveNew();
+        } else {
+            this.update();
+        }
+    }
+
+    private void saveNew() throws SQLException {
+        Connection connect = DBConnection.getConnection();
+        String query = "INSERT INTO personne (NOM,PRENOM) VALUES (?,?)";
+        PreparedStatement stmt = connect.prepareStatement(query);
+        stmt.setString(1, this.nom);
+        stmt.setString(2, this.prenom);
+        stmt.executeUpdate();
+
+        query = "SELECT ID FROM personne WHERE NOM=? AND PRENOM=?";
+        stmt = connect.prepareStatement(query);
+        stmt.setString(1,this.nom);
+        stmt.setString(2,this.prenom);
+        this.id = stmt.executeQuery().getInt("ID");
+    }
+
+    private void update() throws SQLException {
+        Connection connect = DBConnection.getConnection();
+        String query = "UPDATE personne" +
+                "SET NOM=?,PRENOM=?" +
+                "WHERE ID=?";
+        PreparedStatement stmt = connect.prepareStatement(query);
+        stmt.setString(1, this.nom);
+        stmt.setString(2, this.prenom);
+        stmt.setInt(3,this.id);
+        stmt.executeUpdate();
     }
 
 }
