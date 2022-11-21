0c6f2da1d0528a8464481f5525156e1375452bee
2Mo <71017381+Amaury258@users.noreply.github.com>
2022-11-21 16:53:13 +0100

    Exo 7:findAll, findById et findByName

--- activeRecord/src/activeRecord/Personne.java
+++ activeRecord/src/activeRecord/Personne.java
@@ -1,5 +1,8 @@
 package activeRecord;
 
+import java.sql.*;
+import java.util.ArrayList;
+
 public class Personne {
     private int id;
     private String nom, prenom;
@@ -8,6 +11,50 @@ public class Personne {
         id=-1;
         nom=n;
         prenom=p;
+    }
+
+    public int getId(){
+        return id;
+    }
+
+    public static ArrayList<Personne> findAll() throws SQLException {
+        Connection connect=DBConnection.getConnection();
+
+        String queryString = "SELECT * FROM personne";
+        Statement stmt = connect.createStatement();
+        ResultSet rs = stmt.executeQuery(queryString);
+        ArrayList<Personne> tab = new ArrayList<>();
+        while(rs.next()){
+            tab.add(new Personne(rs.getString("NOM"),rs.getString("PRENOM")));
+        }
+        return tab;
+    }
+
+    public static Personne findById(int id) throws SQLException{
+        Connection connect=DBConnection.getConnection();
+
+        Personne p = null;
+        String queryString = "SELECT * FROM personne WHERE ID = ?";
+        PreparedStatement stmt = connect.prepareStatement(queryString);
+        stmt.setInt(1,id);
+        ResultSet rs = stmt.executeQuery(queryString);
+        if(rs.next()){
+            p = new Personne(rs.getString("NOM"),rs.getString("PRENOM"));
+        }
+        return p;
+    }
+
+    public static ArrayList<Personne> findByName(String name) throws SQLException{
+        Connection connect=DBConnection.getConnection();
+
+        String queryString = "SELECT * FROM personne WHERE NOM = ?";
+        PreparedStatement stmt = connect.prepareStatement(queryString);
+        ResultSet rs = stmt.executeQuery(queryString);
+        ArrayList<Personne> tab = new ArrayList<>();
+        while(rs.next()){
+            tab.add(new Personne(rs.getString("NOM"),rs.getString("PRENOM")));
+        }
+        return tab;
     }
 
 }
